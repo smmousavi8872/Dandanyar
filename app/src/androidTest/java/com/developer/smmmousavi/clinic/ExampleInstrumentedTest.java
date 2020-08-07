a2d0d04bfb6e372f71b980038ccb856428dee2f1
smmousavi8872 <s.m.mousavi1993@gmail.com>
2020-08-07 14:17:50 +0430

    app name changed into 'clinic'

--- app/src/androidTest/java/com/developer/smmmousavi/clinic/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/developer/smmmousavi/clinic/ExampleInstrumentedTest.java
@@ -2,13 +2,13 @@ package com.developer.smmmousavi.clinic;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.developer.smmmousavi.dentistry", appContext.getPackageName());
+        assertEquals("com.developer.smmmousavi.clinic", appContext.getPackageName());
     }
 }
